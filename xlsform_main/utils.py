3a12f23475d44ae0c8c0c14ddd30ad23da043214
Ukang'a Dickson <ukanga@gmail.com>
2013-02-25 13:23:24 +0300

    support fileobjects as well as file paths in generating the survey object

--- xlsform_main/utils.py
+++ xlsform_main/utils.py
@@ -1,7 +1,13 @@
+from django.core.files.base import File
+
 from pyxform.builder import create_survey_from_xls
 
 
 def process_xlsform(filepath):
-    with open(filepath) as f:
-        survey = create_survey_from_xls(f)
-        return survey
+    survey = None
+    if isinstance(filepath, File):
+        survey = create_survey_from_xls(filepath)
+    else:
+        with open(filepath) as f:
+            survey = create_survey_from_xls(f)
+    return survey
