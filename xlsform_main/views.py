3e910987831bdedb7433471929b7799e7bae2c6d
Ukang'a Dickson <ukanga@gmail.com>
2013-02-26 12:33:15 +0300

    csrf_exempt

--- xlsform_main/views.py
+++ xlsform_main/views.py
@@ -1,10 +1,12 @@
 from django.utils import simplejson
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 
 from .utils import process_xlsform
 
 
+@csrf_exempt
 @require_POST
 def consume_xlsform(request):
     xlsform_file = request.FILES.pop('xlsform_file', [])
